cfa722df696a1bfa94b654e377606b078cd2aed1
Naveneetha Vasudevan <kvasudevan@smartcommunications.com>
2021-11-07 18:07:27 +0000

    Return the entire sinbad output from invoke

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod sinbad;
 use sinbad::SinBAD;
 use std::env;
 use crate::sinbad::SinBADError;
+use crate::sinbad::SinBADOutput;
 
 pub fn sinbad() -> Result<SinBAD, SinBADError> {
     let timeout_cmd = env::var("TIMEOUT_CMD")
@@ -23,9 +24,33 @@ pub fn sinbad() -> Result<SinBAD, SinBADError> {
 }
 
 pub fn invoke(sin: &SinBAD, duration: usize, backend: &str, depth: usize, gp: &str, lp: &str)
-              -> Result<bool, SinBADError> {
-    let res = sin.invoke(duration, backend, depth, gp, lp)?;
-    Ok(res.is_amb())
+              -> Result<SinBADOutput, SinBADError> {
+    sin.invoke(duration, backend, depth, gp, lp)
 }
 
+#[cfg(test)]
+mod tests {
+    use crate::{sinbad, invoke};
 
+    #[test]
+    /// requires the following environment variables:
+    /// SINBAD_CMD - path to sinbad python script
+    /// TIMEOUT_CMD - path to `timeout` command (mostly `/usr/bin/timeout`)
+    /// ACCENT_DIR - path to the accent accent compiler directory
+    fn test_sinbad() {
+        let backend = "dynamic1";
+        let depth = 10;
+        let gp = "./grammars/amb.acc";
+        let lp = "./grammars/general.lex";
+        let duration: usize = 5;
+        let sin = sinbad()
+            .expect("Unable to create a SinBAD instance!");
+        let res = invoke(&sin, duration, backend, depth, gp, lp);
+        match res {
+            Ok(sin_out) => { println!("{} => {}", gp, sin_out.is_amb()) },
+            Err(e) => {
+                eprintln!("Error:\n{}", e.to_string());
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ pub mod sinbad;
 fn main() {
     let backend = "dynamic1";
     let depth = 10;
-    let gp = "/var/tmp/cfg_ds/20.acc";
-    let lp = "/home/krish/kv/sinbad/bin/general.lex";
+    let gp = "./grammars/amb.acc";
+    let lp = "./grammars/general.lex";
     let duration: usize = 5;
     let sin = sinbad_rs::sinbad()
         .expect("Unable to create a SinBAD instance!");
     let res = sinbad_rs::invoke(&sin, duration, backend, depth, gp, lp);
     match res {
-        Ok(amb) => { println!("{} => {}", gp, amb) },
+        Ok(sin_out) => { println!("{} => {}", gp, sin_out.is_amb()) },
         Err(e) => {
             eprintln!("Error:\n{}", e.to_string());
         }
